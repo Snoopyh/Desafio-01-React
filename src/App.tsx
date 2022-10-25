4271dc2721bc3e984a7c1b99cc895aa102d85b22
Snoopyh <ismaelxd500@gmail.com>
2022-10-24 23:39:14 -0300

    Desafio ToDoList

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { TaskList } from './components/TaskList'
+import { Header } from "./components/Header";
+import './styles/global.scss'
+
+
+function App() {
+
+  return (
+    <>
+    <Header />
+    <TaskList />
+    </>
+  )
+}
+
+export default App
